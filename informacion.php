6473bb8beae106b6433aa15f600cdcde1ccf33c1
aureacomco <jjuansemoreno1019@gmail.com>
2025-08-27 18:48:13 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,11 +1,10 @@
 <?php
 $Identificacion = $_POST['Identificacion'];
-$NomApellidos = $_POST['NomApellidos'];
-$Cargo = $_POST['Cargo'];
-$Salario = $_POST['Salario'];
-
-echo "La identificación del empleado es: " . $Identificacion . "<br>";
-echo "Los nombres y apellidos del empleado son: " . $NomApellidos . "<br>";
-echo "El cargo del empleado es: " . $Cargo . "<br>";
-echo "El salario del empleado es: " . $Salario;
+$NomApellidos= $_POST['NomApellidos'];
+$Cargo= $_POST['Cargo'];
+$Salario= $_POST['Salario'];
+echo "La identificacion del Empleado es: ". $Identificacion  "<br>";
+echo "Los nombres y apellidos del empleado son: ". $NomApellidos . "<br>";
+echo "El cargo del empleado es: ". $Cargo . "<br>";
+echo "El salario del empleado es: ". $Salario . "<br>";
 ?>
